79e58959ac22249052416945078a4316db1235f0
whiterose.jpeg <81488185+redhet-whiterose@users.noreply.github.com>
2025-08-08 18:59:17 +0530

    Update NucleiFuzzer.sh

--- NucleiFuzzer.sh
+++ NucleiFuzzer.sh
@@ -15,7 +15,7 @@ cat << "EOF"
        / / / / /_/ / /__/ /  __/ / __/ /_/ / / /_/ /_/  __/ /    
       /_/ /_/\__,_/\___/_/\___/_/_/  \__,_/ /___/___/\___/_/   v2.5.1
       
-                                     Made by Satya Prakash (0xKayala)
+                                     Made by Redhet (whiterose)
 EOF
 echo -e "${RESET}"
 
